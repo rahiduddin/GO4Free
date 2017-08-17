f96b6a89e60c0153c3cf135b411888c385189855
smiley67 <keonneg@yahoo.com>
2017-08-17 17:51:01 -0400

    daedrtfaha

--- go4free.py
+++ go4free.py
@@ -10,14 +10,7 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('home.html')
         self.response.write(template.render())
-        user = users.get_current_user()
-        if user:
-             #greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' % (user.nickname(), users.create_logout_url('/')))
-             template = jinja_environment.get_template('home.html')
-             self.response.write(template.render({'user': user, 'users': users}))
-        else:
-            greeting = ('<a href="%s">Sign in or register</a>.' % users.create_login_url('/'))
-            self.response.write('<html><body>%s</body></html>' % greeting)
+
 class EventsHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('Events.html')
@@ -38,13 +31,13 @@ class JobsHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 class RelaxtionHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('relaxtion.html')
+        template = jinja_environment.get_template('relaxation.html')
         self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/Events',EventsHandler),
     ('/Career',JobsHandler),
-    ('/relaxtion',RelaxtionHandler),
+    ('/relaxation',RelaxtionHandler),
     ('/maps',MapHandler),
 ], debug=True)
