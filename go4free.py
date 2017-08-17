2e09d7e6b3e5ae7812225da75dd4b4656a40b19e
smiley67 <keonneg@yahoo.com>
2017-08-17 11:20:29 -0400

    made the dataserver

---
+++ go4free.py
@@ -0,0 +1,25 @@
+import jinja2
+import os
+import webapp2
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('home.html')
+        self.response.write(template.render())
+class EventsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('Events.html')
+        self.response.write(template.render())
+class JobsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('Jobs.html')
+        self.response.write(template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/Events',EventsHandler),
+    ('/Career',JobsHandler),
+], debug=True)
