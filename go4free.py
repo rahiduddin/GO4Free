c347a08f18e4cfa91078be099ae8228a724eada5
smiley67 <keonneg@yahoo.com>
2017-08-17 13:02:37 -0400

    loging

--- go4free.py
+++ go4free.py
@@ -1,5 +1,6 @@
 import jinja2
 import os
+from google.appengine.api import users
 import webapp2
 
 jinja_environment = jinja2.Environment(
@@ -7,12 +8,24 @@ jinja_environment = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('home.html')
-        self.response.write(template.render())
+        user = users.get_current_user()
+        if user:
+            #greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' % (user.nickname(), users.create_logout_url('/')))
+            template = jinja_environment.get_template('home.html')
+            self.response.write(template.render({'user': user, 'users': users}))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' % users.create_login_url('/'))
+            self.response.write('<html><body>%s</body></html>' % greeting)
+
+
+        #template = jinja_environment.get_template('home.html')
+        #self.response.write(template.render())
+
 class EventsHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('Events.html')
         self.response.write(template.render())
+
 class JobsHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('Jobs.html')
